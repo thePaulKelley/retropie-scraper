942ebcf2c63c1d8b4f5f8b23e4adede159a469b0
paulkelley <pkelley@gmail.com>
2022-06-29 00:06:53 -0700

    moving from function to a multi-purpose class

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import os
 import xmltodict
 import pandas as pd
 
+
 def scrap_gamelist(roms_folder):
     """
     Traverses the rom folder to get all the gameslist's. Requires target rom direction, from there will only go one
@@ -19,10 +20,10 @@ def scrap_gamelist(roms_folder):
     skip_list = []
 
     # loop through directories and extract out the game metadata and add it to the main dataframe
-    for dir in dirs:
+    for directory in dirs:
         try:
             # Open the file
-            fname = os.path.join(roms_folder, dir, "gamelist.xml")
+            fname = os.path.join(roms_folder, directory, "gamelist.xml")
             f = open(fname)
             gxml = f.read()
 
@@ -35,15 +36,44 @@ def scrap_gamelist(roms_folder):
             # convert the data into a pandas dataframe and add source columns
             df = pd.DataFrame.from_dict(gdata)
             df['system'] = system
-            df['folder'] = dir
+            df['folder'] = directory
             game_df = pd.concat([game_df, df])
 
         except:
             # add skipped folders to the skip array
-            skip_list.append(dir)
+            skip_list.append(directory)
 
     # Converting the dataframe back into a dict to make it more available for future functions
     game_list = game_df.to_dict(orient='records')
     scrap_result = {'game_list': game_list, 'skip_list': skip_list}
 
     return scrap_result
+
+
+class RetroPieScraper:
+    """
+    Tools for creating, updating, and using game_list data as a Pandas dataframe.
+    """
+
+    def __init__(self, roms_folder):
+        self.roms_folder = roms_folder
+        self.skip_list = []
+        self.game_table = pd.DataFrame()
+
+    def _run_scraper(self):
+        # run the scraper internally. Meant to update the skip_list automatically but to save compute costs the results
+        # need to be used/stored by the function that calls this
+        results = scrap_gamelist(self.roms_folder)
+        self.skip_list = results['skip_list']
+        return results['game_list']
+
+    def create_game_table(self):
+        df = pd.DataFrame(self._run_scraper())
+        self.game_table = df
+        shape = df.shape
+        return f'Create game_table with (rows, columns): {shape}'
+
+    def save_game_table(self, target='game_list.csv'):
+        # Exports game_table as a csv.
+        self.game_table.to_csv(target)
+        return f'game_table saved to {target}'
